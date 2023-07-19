f250f6186c31c51a47e3c5fc2ca8431aa993c9ff
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-19 17:40:52 +0200

    task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+
+const formEl = document.querySelector(".feedback-form");
+    
+const formData = {};
+
+ formEl.addEventListener("submit", onSubmitForm);
+ formEl.addEventListener("input", throttle(onInput, 500));
+
+ populateTextarea();
+
+ function onSubmitForm(event){
+    event.preventDefault();
+
+    console.log(`Your email: ${formEl.email.value}`);
+    console.log(`Your message: ${formEl.message.value}`);
+
+    event.currentTarget.reset();    
+
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+ function onInput(event){
+
+    formData[event.target.name]= event.target.value; 
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+   
+ };
+
+function populateTextarea(){
+
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    const parsedMessage = JSON.parse(savedMessage);
+
+    if(parsedMessage){     
+       formEl.email.value = parsedMessage.email;
+       formEl.message.value = parsedMessage.message;
+    }
+};
